39a11ce21e8faad577bc067cdd0e032f00fa3dc7
tobiasberger <wi24b052@technikum-wien.at>
2026-04-15 12:31:45 +0200

    initial commit

---
+++ src/test/java/at/fhtechnikum/disys_project/DisysProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.disys_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisysProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
