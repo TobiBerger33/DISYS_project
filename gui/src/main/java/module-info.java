e869c518c2d9d66e3fe78f8afe7f21f62d8b0b11
Michael Lucuta <ml@newsrooms.ai>
2026-06-22 13:39:51 +0200

    fix gui missing dependencies

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.energycommunity.javafxgui {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
     requires java.net.http;
 
     opens com.energycommunity.javafxgui to javafx.fxml;
